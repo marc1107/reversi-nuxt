6629a052ca38bdf0e5a343be41fcc5c836cf44a5
marc1107 <m.bohner99@gmail.com>
2024-12-03 08:50:18 +0100

    implemented google login

--- store/auth.js
+++ store/auth.js
@@ -20,7 +20,6 @@ export const actions = {
   async login({ commit }, { email, password }) {
     try {
       const { user } = await this.$fire.auth.signInWithEmailAndPassword(email, password);
-      console.log('Logged in:', user);
       commit('setUser', user);
     } catch (error) {
       console.error('Error logging in:', error.message);
@@ -29,8 +28,7 @@ export const actions = {
   async loginWithGoogle({ commit }) {
     try {
       const { user } = await this.$fire.auth.signInWithPopup(googleProvider);
-      console.log('Logged in with Google:', user);
-      commit('setUser', user);
+      commit('setUser', user.displayName);
     } catch (error) {
       console.error('Error logging in with Google:', error.message);
     }
